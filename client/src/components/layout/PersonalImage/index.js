a0ec86fa0dda4d35cca81bb4487c328562a6d3ec
Bailee Dastugue <bailee.dast@gmail.com>
2021-09-09 20:12:09 -0500

    resume section filled out

--- client/src/components/layout/PersonalImage/index.js
+++ client/src/components/layout/PersonalImage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -24,8 +24,9 @@ const useStyles = makeStyles((theme) => ({
     },
     [theme.breakpoints.up(768)]: {
       bottom: '-40px',
-      height: '250px',
-      width: '250px',
+      right: '-15px',
+      height: '270px',
+      width: '270px',
     },
     [theme.breakpoints.up(1024)]: {
       bottom: '-115px',
@@ -33,18 +34,56 @@ const useStyles = makeStyles((theme) => ({
       height: '300px',
       width: '300px',
     },
-    [theme.breakpoints.up(1024)]: {
+    [theme.breakpoints.up(1440)]: {
       bottom: '-155px',
       right: '25px',
       height: '330px',
       width: '330px',
     },
   },
+  cat: {
+    // display: 'none',
+    backgroundColor: theme.palette.primary.light,
+    position: 'absolute',
+    bottom: '0px',
+    right: '95px',
+    height: '40px',
+    width: '40px',
+    borderRadius: '100%',
+    [theme.breakpoints.up(375)]: {
+      bottom: '-10px',
+      right: '105px',
+      height: '45px',
+      width: '45px',
+    },
+    [theme.breakpoints.up(425)]: {
+      bottom: '-40px',
+      right: '125px',
+    },
+    [theme.breakpoints.up(768)]: {
+      bottom: '-55px',
+      right: '200px',
+      height: '110px',
+      width: '110px',
+      backgroundImage:
+        'url(https://res.cloudinary.com/dp9ndx3oj/image/upload/v1631190584/bdastugue/Bellatrix.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'top right',
+      borderColor: theme.palette.primary.light,
+      // margin: '10px',
+      border: '5px solid',
+    },
+  },
 }));
 
 const PersonalImage = () => {
   const classes = useStyles();
-  return <div className={classes.root} />;
+  return (
+    <Fragment>
+      <div className={classes.root} />;
+      <div className={classes.cat} />
+    </Fragment>
+  );
 };
 
 export default PersonalImage;
